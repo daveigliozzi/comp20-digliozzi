73c0529f2c45fd82dbaa150c9564d0d495630994
daveigliozzi <daveigliozzi@gmail.com>
2014-02-24 23:04:23 -0500

    Added the finishing touches

--- duckhunt/README.txt
+++ duckhunt/README.txt
@@ -3,3 +3,9 @@
 //Assignment #2
 //Duckhunt
 //Due Tuesday, Feb. 25th
+
+//I collaborated on this Assingment with Emily Taintor and James Bonish
+
+//I spent about 2 hours on this project
+
+//I believe that all the work has been correctly implemented
--- duckhunt/game.js
+++ duckhunt/game.js
@@ -7,7 +7,17 @@ var ctx = canvas.getContext('2d');
 
 sprite = new Image();
 sprite.src = "assets/duckhunt.png";
+//The tree is first so it will be in the background behind the tall grass
 ctx.drawImage(sprite, 0, 271, 83, 122, 270, 250, 166, 244);
+//the grass and dirt road
 ctx.drawImage(sprite, 0, 720, 800, 180, 0, 420, 800, 180);
+//the sniffing dog
 ctx.drawImage(sprite, 0, 0, 60, 50, 200, 470, 100, 100);
+//birds 1-5
+ctx.drawImage(sprite, 0, 115, 35, 32, 100, 100, 35, 32);
+ctx.drawImage(sprite, 0, 115, 35, 32, 400, 200, 35, 32);
+ctx.drawImage(sprite, 0, 115, 35, 32, 150, 160, 35, 32);
+ctx.drawImage(sprite, 0, 115, 35, 32, 700, 190, 35, 32);
+ctx.drawImage(sprite, 0, 115, 35, 32, 200, 200, 35, 32);
+
 };
