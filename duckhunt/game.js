b4c205dc92bfb523cdeeff8ded056085fa8ed3f6
daveigliozzi <daveigliozzi@gmail.com>
2014-02-24 22:55:14 -0500

    spent a long time trying to figure out how to draw sprites to canvas, had to add the image to teh html file

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,13 @@
 // Your work goes here...
+
+var draw = function(){
+
+var canvas = document.getElementById('game');
+var ctx = canvas.getContext('2d');
+
+sprite = new Image();
+sprite.src = "assets/duckhunt.png";
+ctx.drawImage(sprite, 0, 271, 83, 122, 270, 250, 166, 244);
+ctx.drawImage(sprite, 0, 720, 800, 180, 0, 420, 800, 180);
+ctx.drawImage(sprite, 0, 0, 60, 50, 200, 470, 100, 100);
+};
