da5298c5da7f6c39fc9f094ab670f7afc6829c5b
daveigliozzi <daveigliozzi@gmail.com>
2014-03-13 17:22:44 -0400

    attempting to only draw one line at a time to the canvas

--- transit/mbta.js
+++ transit/mbta.js
@@ -77,7 +77,7 @@ function renderMap()
 	
 	console.log(scheduleData["line"]);
 	for(var i = 0; i < lineData.length; i++){
-
+		console.log(lineData[i]["line"]);
 		if (scheduleData["line"] == lineData[i]["line"]){
 			mark = new google.maps.Marker({
 			position: new google.maps.LatLng(lineData[i]["latitude"], lineData[i]["longitude"]),
