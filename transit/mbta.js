1b10e0af6fc5fd2ec755d154da72248b86769f07
daveigliozzi <daveigliozzi@gmail.com>
2014-03-13 15:07:13 -0400

    switched to gh pages thingy

--- transit/mbta.js
+++ transit/mbta.js
@@ -1,37 +1,3 @@
-/*var xhr;
-
-function init() {
-xhr = new XMLHttpRequest();
-xhr.open("get", "http://mbtamap.herokuapp.com/mapper/rodeo.json", true);
-
-//handles the response
-xhr.onreadystatechange = dataReady;
-xhr.send(null); //Go! Execute!
-
-}
-
-function dataReady(){
-//console.log(xhr.readyState);
-//ready state numbers
-//0 = not initialized
-//1 = set up
-//2 = sent
-//3 = in progress
-//4 = complete
-
-if(xhr.readyState == 4){
-	scheduleData = JSON.parse(xhr.responseText);
-	scheduleDom = document.getElementById("schedule");
-	scheduleDom.innerHTML = scheduleData["line"];
-}
-else if(xhr.readyState == 4 && xhr.status == 500){
-	scheduleDom = document.getElementById("schedule");
-	schduleDom.innterHTML = '<p><img alt = "alternative picture for error" src= http://mavdig.com/images/blog_header/the-dude-abides-christian-broadbent.jpg </img></p>'
-}
-
-
-
-}*/
 
 //From prof. Ming Chow's example
 
@@ -53,6 +19,31 @@ function init()
 {
 	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 	getMyLocation();
+
+	request.open("get", "http://mbtamap.herokuapp.com/mapper/rodeo.json", true);
+
+	//handles the response
+	request.onreadystatechange = dataReady;
+	request.send(null); //Go! Execute!
+}
+
+function dataReady(){
+//console.log(xhr.readyState);
+//ready state numbers
+//0 = not initialized
+//1 = set up
+//2 = sent
+//3 = in progress
+//4 = complete
+
+if(request.readyState == 4){
+	scheduleData = JSON.parse(request.responseText);
+	scheduleDom = document.getElementById("schedule");
+	scheduleDom.innerHTML = scheduleData["line"];
+}
+else if(request.readyState == 4 && request.status == 500){
+	scheduleDom = document.getElementById("schedule");
+	schduleDom.innterHTML = '<p><img alt = "alternative picture for error" src= http://mavdig.com/images/blog_header/the-dude-abides-christian-broadbent.jpg </img></p>'
 }
 
 function getMyLocation()
