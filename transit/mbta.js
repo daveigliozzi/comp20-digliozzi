511904f1dc9d63feefc4caaeb64e513934a0abb1
daveigliozzi <daveigliozzi@gmail.com>
2014-03-13 23:56:34 -0400

    attempting to debug random errors

--- transit/mbta.js
+++ transit/mbta.js
@@ -100,12 +100,11 @@ function loopLines()
 function createMarker(i)
 {
 
-	if (lineData[i]["line"].toLowerCase == "red")
-	{
-		mark = new google.maps.Marker({
+	if(lineData[i]["line"] == "Blue")
+		{
+			mark = new google.maps.Marker({
 			position: new google.maps.LatLng(lineData[i]["latitude"], lineData[i]["longitude"]),
-			title: lineData[i]["name"],
-			icon: "flag1.png"
+			title: lineData[i]["name"]
 			});
 			mark.setMap(map);
 
@@ -113,12 +112,28 @@ function createMarker(i)
 			infowindow.setContent(mark.title);
 			infowindow.open(map, mark);	
 			});
-	}
-	else
-	{
-		mark = new google.maps.Marker({
+
+
+			if(i > 0 && lineData[i]["line"] == lineData[i-1]["line"]){
+
+
+				var segment = [mark.position, new google.maps.LatLng(lineData[i-1]["latitude"], lineData[i-1]["longitude"])];
+				path = new google.maps.Polyline({
+				path: segment,
+				strokeColor: "#0000FF",
+				strokeOpacity: 1.0,
+				strokeWeight: 2
+				});
+				path.setMap(map);
+			}
+		}
+
+		if(lineData[i]["line"] == "Red")
+		{
+			mark = new google.maps.Marker({
 			position: new google.maps.LatLng(lineData[i]["latitude"], lineData[i]["longitude"]),
 			title: lineData[i]["name"],
+			//icon: "flag1.png"
 			});
 			mark.setMap(map);
 
@@ -126,7 +141,56 @@ function createMarker(i)
 			infowindow.setContent(mark.title);
 			infowindow.open(map, mark);	
 			});
-	}
+			if(lineData[i]["name"] == "Savin Hill"){
+				var segment = [mark.position, new google.maps.LatLng(42.320685, -71.052391)];
+				path = new google.maps.Polyline({
+				path: segment,
+				strokeColor: "#FF0000",
+				strokeOpacity: 1.0,
+				strokeWeight: 2
+				});
+				path.setMap(map);
+
+			}
+			else if (i > 0 && lineData[i]["line"] == lineData[i-1]["line"])
+			{
+				var segment = [mark.position, new google.maps.LatLng(lineData[i-1]["latitude"], lineData[i-1]["longitude"])];
+				path = new google.maps.Polyline({
+				path: segment,
+				strokeColor: "#FF0000",
+				strokeOpacity: 1.0,
+				strokeWeight: 2
+				});
+				path.setMap(map);
+			}
+			
+		}
+
+		else (lineData[i]["line"] == "Orange")
+		{
+
+			var mark;
+			mark = new google.maps.Marker({
+			position: new google.maps.LatLng(lineData[i]["latitude"], lineData[i]["longitude"]),
+			title: lineData[i]["name"]
+			});
+			mark.setMap(map);
+
+			google.maps.event.addListener(mark, 'click', function() {
+			infowindow.setContent(mark.title);
+			infowindow.open(map, mark);	
+			});
+			if(i > 0 && lineData[i]["line"] == lineData[i-1]["line"]){
+				var segment = [mark.position, new google.maps.LatLng(lineData[i-1]["latitude"], lineData[i-1]["longitude"])];
+				path = new google.maps.Polyline({
+				path: segment,
+				strokeColor: "#FF6600",
+				strokeOpacity: 1.0,
+				strokeWeight: 2
+				});
+				path.setMap(map);
+			}
+		}
 
 }
 
